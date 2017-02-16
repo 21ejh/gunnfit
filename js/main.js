443f98cc8e94a3cd0e23556c21c89e82e1778a3e
Joey Hanlon <joey.hanlon@jbeaudoin-mbpr.enterprise.886ventures.com>
2017-02-16 14:12:44 -0500

    Bios

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
+$(document).ready(function(){
+
+	$('.xtoggle').click(function(){
+		var scroll = $(window).scrollTop();
+
+		$(this).toggleClass('xtoggleOpen');
+
+    	if (scroll < 100) {
+        	$('.navbar').toggleClass('navbarActivated');
+    	}
+
+	});
+
+	$(window).scroll(function() {    
+    	var scroll = $(window).scrollTop();
+
+    	if (scroll >= 100) {
+        	$('.navbar').addClass('navbarOpen');
+    	} else {
+        	$('.navbar').removeClass('navbarOpen');
+    	}
+	});
+
+});
 
